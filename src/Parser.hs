461ac25ae33808803d3e0bae77dcca47f35696ac
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2022-03-17 16:42:34 +0100

    decided on a parsing library and a strategy for intentation sensitivity

--- src/Parser.hs
+++ src/Parser.hs
@@ -18,6 +18,36 @@ module Parser where
 
 import Ast
 import Text.Parsec
+import Text.Parsec.Char ( lower, upper )
+
+type Indentation = Int
+type Parser      = Parsec String Indentation
+
+lexeme :: Parser a -> Parser a
+lexeme p = p >>= \a -> many space >> return a
+
+reserved :: [ Name ]
+reserved = [ "let", "in", "rlet", "case", "dup" ]
+
+isReserved :: Name -> Bool
+isReserved name = name `elem` reserved
+
+underscore :: Parser Char
+underscore = char '_'
+
+name :: Parser Name
+name =
+  do head <- lower
+     tail <- many (letter <|> digit <|> underscore)
+     return (head : tail)
+
+ident :: Parser Name
+ident = try $
+  do f <- name
+     if     isReserved f
+       then fail $ "unexpected keyword : " ++ f
+       else return f
+
 
 -- Todo:
 -- [ ] Syntactic abbreviations for lists and pairs.
