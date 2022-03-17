a66ee89ea8c07112297cb82e5743c3f7b6d9ca35
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2022-03-17 15:29:52 +0100

    ast as far as I understand

--- src/Ast.hs
+++ src/Ast.hs
@@ -19,3 +19,35 @@ Gent, Beligium, July 2022).
 
 module Ast where
 
+type Name       = String
+type FName      = Name
+type VName      = Name
+type Program    = [Definition]
+type Body       = Expression
+type InPattern  = Pattern
+type OutPattern = Pattern
+
+newtype Definition
+  = Function { f :: FName, p :: Pattern, e :: Body }
+  deriving (Show, Eq)
+
+data Pattern
+  = Variable    VName
+  | Constructor Name [Pattern]
+  | Duplicate   Pattern
+  deriving (Show, Eq)
+
+data Expression
+  = Pattern Pattern
+  | Let  OutPattern FName InPattern  Body
+  | RLet InPattern  FName OutPattern Body
+  | Case Pattern [(Pattern, Body)]
+  deriving (Show, Eq)
+
+data Value
+  = Value Name [Value] -- Algebraic data type.
+  deriving (Show, Eq)
+
+-- Todo:
+-- [ ] Pretty printer?
+-- [ ] What instances.
