aa5a3780b87877b28cdac5916f19f016848b63e6
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2022-03-17 15:08:30 +0100

    initial formalities (let's talk about the importance of this

---
+++ src/Ast.hs
@@ -0,0 +1,21 @@
+{-|
+
+Module      : Ast
+Description : The abstract syntax for RCPL goes here.
+Copyright   : Joachim Tilsted Kristensen
+              Michael Kirkedal
+              Eric Jul
+Licence     : TBA
+Maintainer  : tilsted@di.ku.dk
+Stability   : experimental
+Portability : POSIX
+
+The current syntax of the reversible programming language suggested in
+the article, `Towards a Reversible Functional Language`, presented at
+the third international workshop on reversible computation (RC 2011, in
+Gent, Beligium, July 2022).
+
+-}
+
+module Ast where
+
