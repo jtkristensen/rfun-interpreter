b1224043deda1c71ab7269e48403296e7a455f8f
FurryRogue <joachim.tilsted@gmail.com>
2022-04-03 23:34:24 +0200

    better documentation

--- test/Core/AnalysisTests.hs
+++ test/Core/AnalysisTests.hs
@@ -20,9 +20,11 @@ coreAnalysisTests =
     , negativeBindingsAnalysisTests
     ]
 
-run :: Source -> Either ParseError (Program SourceFileReference)
-run = runParser program () "stdin"
+-- Parse a program string representation.
+parse :: Source -> Either ParseError (Program SourceFileReference)
+parse = runParser program () "stdin"
 
+-- Expect a program analysis to result from parsing and analysing a program.
 expectProgram ::
   (Eq e, Show e, Eq a, Show a)
     => (Program SourceFileReference -> Except e a) -- The analysis.
@@ -30,11 +32,12 @@ expectProgram ::
     -> Source                                      -- A source program.
     -> Either e a                                  -- The expected analysis result.
     -> Assertion
-expectProgram analysis normalize src expected =
-  case run src of
+expectProgram analysis normalise program expected =
+  case parse program of
     (Left  err) -> assertBool ("<error>: " ++ show err) False
-    (Right ast) -> normalize (runExcept $ analysis ast) @?= normalize expected
+    (Right ast) -> normalise (runExcept $ analysis ast) @?= normalise expected
 
+-- Make brief "thumbnail" program for test-case titles.
 titleOf :: Source -> String
 titleOf src =
     if   length src' > 60
@@ -46,17 +49,25 @@ titleOf src =
     minimalSpacing (  s : rc     ) = s : minimalSpacing rc
     minimalSpacing _               = ""
 
+-- Expect a program text to pass the bindings analysis.
 passesBindings :: Source -> TestTree
 passesBindings src =
-  testCase (titleOf src) $ expectProgram runBindingsAnalysis id src (return ())
+  testCase (titleOf src) $
+    expectProgram
+      runBindingsAnalysis id src (return ())
 
+-- Expect a program to fail the bindings analysis.
 failsBindings :: Source -> TestTree
 failsBindings src =
-    testCase (titleOf src) $ expectProgram runBindingsAnalysis norm src (Left (return []))
+    testCase (titleOf src) $
+      expectProgram
+        runBindingsAnalysis norm src (Left (return []))
   where
+    -- We don't expect a specific error, but it should not be a ParserError.
     norm (Left (Right _)) = Left $ return []
     norm other            = other
 
+-- A unit test harness of programs that should pass the bindings analysis.
 positiveBindingsAnalysisTests =
   testGroup "Unit tests for programs that should pass bindings analysis" $
     map passesBindings
@@ -74,6 +85,7 @@ positiveBindingsAnalysisTests =
       , "fun inc n = rlet n = s n in n"
       ]
 
+-- A unit test harness of programs that should fail the bindings analysis.
 negativeBindingsAnalysisTests =
   testGroup "Unit tests for programs that should fail bindings analysis" $
     map failsBindings
@@ -82,4 +94,5 @@ negativeBindingsAnalysisTests =
       , "fun f (Pair x x) = IrregularPattern"
       , "fun f x = DefinedButNotUsed"
       , "fun f UsedButNotDefined = x"
+      , "fun inc n = let  n = s m in m"
       ]
