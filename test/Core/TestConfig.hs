3a3248acf74571909094e0d6fc8489da0c6127e1
FurryRogue <joachim.tilsted@gmail.com>
2022-04-07 19:22:34 +0200

    now it is idempotent

--- test/Core/TestConfig.hs
+++ test/Core/TestConfig.hs
@@ -4,5 +4,4 @@ module Core.TestConfig where
 -- This is an argument for QuickCheck, if it is larger, then the testcases
 -- are larger, but they take significantly more time to run.
 sizeOfGeneratedPatterns :: Int
-sizeOfGeneratedPatterns = 3
-
+sizeOfGeneratedPatterns = 6
